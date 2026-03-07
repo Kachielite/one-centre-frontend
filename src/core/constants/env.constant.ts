fd622aadc71a79cfa518a4ad55d55c986b057d87
Kachielite <derrick.onyekachi@gmail.com>
2026-03-07 09:23:05 +0300

    feat(auth): implement zustand for authentication state management and storage

--- src/core/constants/env.constant.ts
+++ src/core/constants/env.constant.ts
@@ -1,5 +1,6 @@
 const ENV = {
   BASE_URL: import.meta.env.VITE_BACKEND_API_URL as string,
+  STORAGE_KEY: import.meta.env.VITE_STORAGE_KEY as string,
 }
 
 export default ENV
---
+++ src/feature/auth/state/auth.state.ts
@@ -0,0 +1,34 @@
+import type { IAuthModel } from "@/feature/auth/types/auth.model.ts"
+import { create } from "zustand/react"
+import { createJSONStorage, persist } from "zustand/middleware/persist"
+import zustandStorage from "@/core/utils/zustand-storage.ts"
+import ENV from "@/core/constants/env.constant.ts"
+
+type AuthStore = {
+  token: IAuthModel | null
+  setToken: (value: IAuthModel) => void
+  clearToken: () => void
+}
+
+const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      token: null,
+      setToken: (value: IAuthModel) => {
+        set({ token: value })
+        // Sync with storage immediately for axios interceptor
+        zustandStorage.setItem(ENV.STORAGE_KEY, JSON.stringify(value))
+      },
+      clearToken: () => {
+        set({ token: null })
+        zustandStorage.removeItem(ENV.STORAGE_KEY)
+      },
+    }),
+    {
+      name: ENV.STORAGE_KEY,
+      storage: createJSONStorage(() => zustandStorage),
+    }
+  )
+)
+
+export default useAuthStore

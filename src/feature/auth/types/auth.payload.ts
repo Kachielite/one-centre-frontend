e5519395e110c0c7dd5baa4ba9d6450e25eb4ad7
Kachielite <derrick.onyekachi@gmail.com>
2026-03-07 18:59:55 +0300

    feat(auth): update email validation to use string type and enhance password confirmation check

--- src/feature/auth/types/auth.payload.ts
+++ src/feature/auth/types/auth.payload.ts
@@ -5,7 +5,7 @@ import {
 } from "@/feature/auth/types/auth.enum.ts"
 
 export const createUserPayloadSchema = z.object({
-  email: z.email("Invalid email address"),
+  email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
   name: z.string().min(2, "Name must be at least 2 characters long"),
 })
@@ -13,29 +13,29 @@ export const createUserPayloadSchema = z.object({
 export type CreateUserPayload = z.infer<typeof createUserPayloadSchema>
 
 export const loginUserPayloadSchema = z.object({
-  email: z.email("Invalid email address"),
+  email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 })
 
 export type LoginUserPayload = z.infer<typeof loginUserPayloadSchema>
 
 export const verifyOtpPayloadSchema = z.object({
-  email: z.email("Invalid email address"),
+  email: z.string().email("Invalid email address"),
   otp: z.string().min(6, "OTP must be at least 6 characters long"),
-  type: z.enum(OtpTypeEnum),
+  type: z.enum(OtpTypeEnum as unknown as [string, ...string[]]),
 })
 
 export type VerifyOtpPayload = z.infer<typeof verifyOtpPayloadSchema>
 
 export const verifyEmailPayloadSchema = z.object({
-  email: z.email("Invalid email address"),
+  email: z.string().email("Invalid email address"),
 })
 
 export type VerifyEmailPayload = z.infer<typeof verifyEmailPayloadSchema>
 
 export const resetPasswordPayloadSchema = z
   .object({
-    email: z.email("Invalid email address"),
+    email: z.string().email("Invalid email address"),
     newPassword: z
       .string()
       .min(6, "New password must be at least 6 characters long"),
@@ -44,15 +44,21 @@ export const resetPasswordPayloadSchema = z
       .min(6, "Confirm password must be at least 6 characters long"),
     otp: z.string().min(6, "OTP must be at least 6 characters long"),
   })
-  .refine((data) => data.newPassword === data.confirmPassword, {
-    message: "Passwords do not match",
+  .superRefine((data, ctx) => {
+    if (data.newPassword !== data.confirmPassword) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+      })
+    }
   })
 
 export type ResetPasswordSchema = z.infer<typeof resetPasswordPayloadSchema>
 export type ResetPasswordPayload = Omit<ResetPasswordSchema, "confirmPassword">
 
 export const socialLoginPayloadSchema = z.object({
-  provider: z.enum(AuthProviderEnum),
+  provider: z.enum(AuthProviderEnum as unknown as [string, ...string[]]),
   idToken: z.string().min(1, "Token is required"),
 })
 

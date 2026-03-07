f3105b50307f146bdf0d443dcfd4d382e0e6a000
Kachielite <derrick.onyekachi@gmail.com>
2026-03-07 09:26:55 +0300

    feat(auth): integrate react-query for data fetching and add useRegister hook

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,23 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/core/components/theme-provider.tsx"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+    },
+  },
+})
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 )
